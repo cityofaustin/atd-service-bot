4f8826a58ee170ad4cae5b0345920e60cd01c83a
John Clary <john.clary@austintexas.gov>
2019-08-09 15:18:56 -0500

    format issues with markdown

--- intake.py
+++ intake.py
@@ -1,10 +1,20 @@
 # TODO: handle "other" division, request type
-# TODO: map to geospatial—document
 # TODO: document geospatial app selection importance
 # TODO: document why email/name are short text
 # TODO: test: all apps, all request types, all divisions
-# TODO: test default assignees
-# TODO: attachment
+# TODO: and cli_args() for deployer
+# TODO: return records_processed for job logging
+# TODO: email confirmation messages
+# TODO: set IT support issues to close in github, setup email to scott < no, to complex. not sending to github
+# TODO: Add workgroup + type to issue title, with truncated Description.
+# TODO: move outcome above workaround in issue description
+# TODO: hide "how soon do you need this" from feature
+# TODO: Change text on "How soon do you need this?" 
+# TODO: move "How would you rate the impact" to the bottom
+# TODO: everything else: hide impact and need questions. and if need is "urgent" then map to "Impact 1: Severe"
+# TODO: change the "how soon do you need this drop-down to radio buttons"
+# TODO: leave application seletor and URL on "everything else"
+# TODO: don't assign amenity to amanda issues except severe
 
 import pdb
 from pprint import pprint as print
@@ -38,6 +48,7 @@ def map_issue(issue, fields, knack_field_map):
         "github_url": None,
         "knack_id": None,
         "repo": None,
+        "state": "Closed"
     }
 
     for field in fields:
@@ -51,9 +62,16 @@ def map_issue(issue, fields, knack_field_map):
                 if not issue[knack_field_label]:
                     continue
 
-                new_value = (
-                    f"{old_value}{knack_field_label}: {issue[knack_field_label]}\n\n"
-                )
+                value = issue[knack_field_label]
+
+                if field.get("format") == "quote_text":
+                    label = f"{knack_field_label}\n"
+                    value = f"> {value}\n\n"
+
+                    new_value = f"{old_value}{label}{value}"
+
+                else:
+                    new_value = f"{old_value}{knack_field_label}: {value}\n\n"
 
                 github_issue[field["github"]] = new_value
 
@@ -73,18 +91,27 @@ def map_issue(issue, fields, knack_field_map):
 
             # get the transform function
             transform_func = getattr(_transforms, field["transform"])
-            transformed = transform_func(untransformed)
+            transformed_value = transform_func(untransformed)
 
             # now merge
             old_value = github_issue[field["github"]]
 
             if field.get("rename"):
 
-                # rename the field label that will be formatted into the merge
-                new_value = f"{old_value}{field.get('rename')}: {transformed}\n\n"
+                knack_field_label = field.get('rename')
+
+            if field.get("format") == "no_label":
+                 new_value = f"{old_value}{transformed_value}\n\n"
+            
+            elif field.get("format") == "quote_text":
+                label = f"{knack_field_label}\n"
+                
+                value = f"> {transformed_value}\n\n"
+
+                new_value = f"{old_value}{label}{value}"
 
             else:
-                new_value = f"{old_value}{knack_field_label}: {transformed}\n\n"
+                new_value = f"{old_value}{knack_field_label}: {transformed_value}\n\n"
 
             github_issue[field["github"]] = new_value
 
@@ -97,7 +124,7 @@ def map_issue(issue, fields, knack_field_map):
                 continue
 
             if not val_knack:
-                # handle empty strings in knack data
+                # handle empty strings in knack data by ignoring them
                 continue
 
             else:
@@ -106,7 +133,8 @@ def map_issue(issue, fields, knack_field_map):
             for val in val_knack:
                 val_mapped = field["map"][val]
 
-                github_issue[field["github"]].append(val_mapped)
+                if val_mapped:
+                    github_issue[field["github"]].append(val_mapped)
 
         elif field["method"] == "map":
 
@@ -133,7 +161,11 @@ def map_issue(issue, fields, knack_field_map):
                 if not issue[knack_field_label]:
                     continue
 
-                val_knack = str(issue[knack_field_label]) + "\n\n"
+                if field.get("format") == "none":
+                    val_knack = issue[knack_field_label]
+
+                else:
+                    val_knack = str(issue[knack_field_label]) + "\n\n"
 
             except KeyError:
                 continue
@@ -159,6 +191,9 @@ def get_repo(g, repo, org="cityofaustin"):
 def main():
 
     issues = get_service_requests(KNACK_APP, API_KEY)
+
+    if not issues.data:
+        return 0
 
     prepared = {}
 
@@ -195,18 +230,18 @@ def main():
                 body=issue["description"],
             )
 
-            pdb.set_trace()
-
             knack_record_update = {
                 "id": issue["knack_id"],
-                "field_367": result.number,  # github issuer number
-                "field_368": repo_name,  # repo name
-                "field_366": "Successful",  # github migration result
+                # "field_394": result.number,  # github issue number
+                "field_395" : issue["repo"], # repo
+                "field_392": "Sent",  # github transmission status
             }
 
+            pdb.set_trace()
+            # update knack record as "Sent"
             response = knackpy.record(
                 knack_record_update,
-                obj_key="object_6",
+                obj_key=KNACK_APP["ref_obj"][0],
                 app_id=KNACK_APP["app_id"],
                 api_key=API_KEY,
                 method="update",
