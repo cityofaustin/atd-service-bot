6d9189b9db7371a5ac05ccd98a8836317b563de6
John Clary <john.clary@austintexas.gov>
2019-08-09 23:50:58 -0500

    update record via forms api

--- intake.py
+++ intake.py
@@ -9,33 +9,38 @@
 # TODO: Add workgroup + type to issue title, with truncated Description.
 # TODO: move outcome above workaround in issue description
 # TODO: hide "how soon do you need this" from feature
-# TODO: Change text on "How soon do you need this?" 
+# TODO: Change text on "How soon do you need this?"
 # TODO: move "How would you rate the impact" to the bottom
 # TODO: everything else: hide impact and need questions. and if need is "urgent" then map to "Impact 1: Severe"
 # TODO: change the "how soon do you need this drop-down to radio buttons"
 # TODO: leave application seletor and URL on "everything else"
 # TODO: don't assign amenity to amanda issues except severe
+# TODO: instead of updating knackpy records by object, submit a form that triggers an email. Wow!
+# TODO: add credentials to 1pass
 
 import pdb
 from pprint import pprint as print
 
 from github import Github
 import knackpy
+import requests
 
 from config.config import KNACK_APP, FIELDS
-from config.secrets import API_KEY, GITHUB_USER, GITHUB_PASSWORD
+from config.secrets import (
+    API_KEY,
+    GITHUB_USER,
+    GITHUB_PASSWORD,
+    KNACK_USERNAME,
+    KNACK_PASSWORD,
+)
 import _transforms
 
 
-def get_service_requests(knack_config, api_key):
+def get_service_requests(scene, view, ref_obj, app_id, api_key):
     # Just a wrapper. Returns a Knackpy object.
 
     return knackpy.Knack(
-        scene=knack_config["scene"],
-        view=knack_config["view"],
-        app_id=knack_config["app_id"],
-        api_key=api_key,
-        ref_obj=knack_config["ref_obj"],
+        scene=scene, view=view, app_id=app_id, api_key=api_key, ref_obj=ref_obj
     )
 
 
@@ -48,7 +53,7 @@ def map_issue(issue, fields, knack_field_map):
         "github_url": None,
         "knack_id": None,
         "repo": None,
-        "state": "Closed"
+        "state": "Closed",
     }
 
     for field in fields:
@@ -98,14 +103,14 @@ def map_issue(issue, fields, knack_field_map):
 
             if field.get("rename"):
 
-                knack_field_label = field.get('rename')
+                knack_field_label = field.get("rename")
 
             if field.get("format") == "no_label":
-                 new_value = f"{old_value}{transformed_value}\n\n"
-            
+                new_value = f"{old_value}{transformed_value}\n\n"
+
             elif field.get("format") == "quote_text":
                 label = f"{knack_field_label}\n"
-                
+
                 value = f"> {transformed_value}\n\n"
 
                 new_value = f"{old_value}{label}{value}"
@@ -188,9 +193,39 @@ def get_repo(g, repo, org="cityofaustin"):
     return g.get_repo(f"{org}/{repo}")
 
 
+def get_token(email, pw, app_id):
+    # get knack app token for forms api
+    data = {"email": email, "password": pw}
+    url = f"https://api.knack.com/v1/applications/{app_id}/session"
+    headers = {"Content-Type": "application/json"}
+    res = requests.post(url, headers=headers, json=data)
+    res.raise_for_status()
+    return res.json()["session"]["user"]["token"]
+
+
+def form_submit(token, app_id, scene, view, payload):
+    record_id = payload["id"]
+
+    url = f"https://api.knack.com/v1/pages/{scene}/views/{view}/records/{record_id}"
+
+    headers = {"X-Knack-Application-Id": app_id, "Authorization": token}
+
+    res = requests.put(url, headers=headers, json=payload)
+
+    res.raise_for_status()
+
+    return res
+
+
 def main():
 
-    issues = get_service_requests(KNACK_APP, API_KEY)
+    issues = get_service_requests(
+        KNACK_APP["api_view"]["scene"],
+        KNACK_APP["api_view"]["view"],
+        KNACK_APP["api_view"]["ref_obj"],
+        KNACK_APP["app_id"],
+        API_KEY,
+    )
 
     if not issues.data:
         return 0
@@ -223,29 +258,35 @@ def main():
 
         for issue in prepared[repo_name]:
 
-            result = repo.create_issue(
-                title=issue["title"],
-                labels=issue.get("labels"),
-                assignees=issue.get("assignee"),
-                body=issue["description"],
-            )
+            # result = repo.create_issue(
+            #     title=issue["title"],
+            #     labels=issue.get("labels"),
+            #     assignees=issue.get("assignee"),
+            #     body=issue["description"],
+            # )
 
-            knack_record_update = {
+            knack_payload = {
                 "id": issue["knack_id"],
                 # "field_394": result.number,  # github issue number
-                "field_395" : issue["repo"], # repo
+                "field_395": issue["repo"],  # repo
                 "field_392": "Sent",  # github transmission status
             }
 
+            # update knack record as "Sent" using form API, which will
+            # trigger an email notificaiton
             pdb.set_trace()
-            # update knack record as "Sent"
-            response = knackpy.record(
-                knack_record_update,
-                obj_key=KNACK_APP["ref_obj"][0],
-                app_id=KNACK_APP["app_id"],
-                api_key=API_KEY,
-                method="update",
+
+            token = get_token(KNACK_USERNAME, KNACK_PASSWORD, KNACK_APP["app_id"])
+
+            response = form_submit(
+                token,
+                KNACK_APP["app_id"],
+                KNACK_APP["api_form"]["scene"],
+                KNACK_APP["api_form"]["view"],
+                knack_payload,
             )
+
+            pdb.set_trace()
 
             responses.append(response)
 
