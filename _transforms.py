8159d846151c06025a6713e4b7a4f6dce492fa79
Frank Hereford <github@frankhereford.com>
2026-07-17 13:41:05 -0500

    Remove unused functions

--- _transforms.py
+++ _transforms.py
@@ -2,29 +2,10 @@ from datetime import datetime
 import math
 
 
-def mills_to_timestamp(mills):
-    return datetime.fromtimestamp(mills / 1000)
-
-
-def app_url(record_id):
-    return f"https://atd.knack.com/dts#service-requests/view-issue-details/{record_id}"
-
-
 def knack_issue_url(record_id, request_id=None):
     url = f"https://atd.knack.com/dts#manage-service-requests/triage-service-request/{record_id}/"
     label = request_id or record_id
     return f"[DTS Service Request Details - {label}]({url})"
-
-
-# courtesy of https://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
-def convert_size(size_bytes):
-    if size_bytes == 0:
-        return "0B"
-    size_name = ("b", "kb", "mb", "gb", "tb", "pb", "eb", "zb", "yb")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return f"{s}{size_name[i]}"
 
 
 def parse_attachment_url(_obj):
